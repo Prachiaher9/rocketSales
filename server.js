6171f063646b9cc79ad475fe9fcdf814abb2afbc
prachi <prachiaher86052@gmai.com>
2024-08-26 17:35:50 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();  // Ensure this is at the very top
+
 const express = require("express");
 const app = express();
-require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -11,15 +12,16 @@ require("./db/db");
 app.use(cors());
 app.use(bodyParser.json());
 
+// Import routes
+const employeeRoutes = require("./routes/employeeRoute");
+
+// Use routes
+app.use("/employee", employeeRoutes);
+
 // Define the port to use
 const PORT = process.env.PORT || 3000;
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 });
-
-  // Import routes
-const employeeRoutes = require("./routes/employeeRoute");
-
-// Use routes
-app.use("/employee",employeeRoutes);
